ac960e95c28e7238170401789fa8b788afd9563c
MikeYoulend <mikeyoulend@gmail.com>
2024-04-02 18:22:54 +0200

    Dom Attribute

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 const Google = document.querySelector("ul li a").style.color = "red"
 const buttonBg = document.querySelector("button").style.backgroundColor = "yellow"
+
+function Fontsize(){
+    document.querySelector("h1").classList.toggle("HUGE")
+}
+ 
+Fontsize();
+
+//prendo l'attributo
+const getAttribute = document.querySelector("a").getAttribute("href")
+console.log(getAttribute);
+
+//setto attributo 
+const setAttribute = document.querySelector("a").setAttribute("href", "https://www.bing.com")
